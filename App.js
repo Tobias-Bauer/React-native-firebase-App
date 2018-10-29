b2ce5cf9806b7c0b93fa56af19bd1b46faf541c7
Tobias-Bauer <tobias.bauer@mbtj.de>
2018-10-29 11:43:41 +0100

    Started building Login screen
    Signed-off-by: Tobias-Bauer <tobias.bauer@mbtj.de>

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert,
+  TouchableOpacity 
+} from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+EStyleSheet.build({
+  $textColor: 'white'
+});
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Thats the first line of text!</Text>
+        <Text style={styles.loginText}>Thats the first line of text!</Text>
+        <TouchableOpacity
+          style={styles.loginScreenButton}
+          onPress={() => navigate('HomeScreen')}
+          underlayColor='#fff'>
+          <Text style={styles.loginButtonText}>Login</Text>
+          </TouchableOpacity>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
+const styles = EStyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
   },
+  loginScreenButton:{
+    right: '20%',
+    left: '80% - 70',
+    top: '80% - 40',
+    bottom: '20%' ,
+    backgroundColor:'blue',
+    borderRadius:10,
+    position: 'absolute',
+    justifyContent:'center'
+  },
+  loginButtonText:{
+      color:'$textColor',
+      textAlign:'center'
+  },
+  loginText:{
+    right: '50% - 80',
+    left: '50% - 80',
+    top: '20%',
+    bottom: '80% - 30 '
+  }
 });
